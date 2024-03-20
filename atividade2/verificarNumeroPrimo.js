ef3cd0e8a279d2340d4c5f7a360c9db45a613dc5
UCBGABRIEL <103432588+UCBGABRIEL@users.noreply.github.com>
2024-03-20 09:43:39 -0300

    Add files via upload

---
+++ atividade2/verificarNumeroPrimo.js
@@ -0,0 +1,59 @@
+function verificarNumeroPrimo(numero) {
+
+if (numero <= 1) {
+
+return false;
+
+}
+
+if (numero <= 3) {
+
+return true;
+
+}
+
+if (numero % 2 === 0 || numero % 3 === 0) {
+
+return false;
+
+}
+
+let i = 5; // A variável i é inicializada com 5, que é o primeiro número primo após 3.
+
+while (i * i <= numero) {
+
+if (numero % i === 0 || numero % (i + 2) === 0) {
+
+return false;
+
+}
+
+i += 6; // Adicionar 6 a i a cada iteração, estamos verificando apenas os números que podem ser primos.
+
+}
+
+return true;
+
+}
+
+
+console.log(verificarNumeroPrimo(0)); // false
+
+console.log(verificarNumeroPrimo(1)); // false
+
+console.log(verificarNumeroPrimo(2)); // true
+
+console.log(verificarNumeroPrimo(3)); // true
+
+console.log(verificarNumeroPrimo(7)); // true
+
+console.log(verificarNumeroPrimo(83)); // true
+
+console.log(verificarNumeroPrimo(100)); // false
+
+console.log(verificarNumeroPrimo(991)); // true
+
+console.log(verificarNumeroPrimo(104729)); // true
+
+console.log(verificarNumeroPrimo(14348907)); // false
+
