1a29e248664418dc0adee6032f09dc78153e8c00
UCBGABRIEL <103432588+UCBGABRIEL@users.noreply.github.com>
2024-05-02 00:03:28 -0300

    Add files via upload

---
+++ atividade6/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'assets')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.post('/dados', (req, res) => {
+  const { nome, cep, logradouro, bairro, numero, complemento, referencia, cidade, estado, telefone, dia, mes, ano } = req.body;
+  console.log("\nNome:", nome, "\nCep:", cep, "\nLogradouro:", logradouro, "\nBairro:", bairro, "\nNúmero:", numero, "\nComplemento:", complemento, "\nReferência:", referencia, "\nCidade:", cidade, "\nEstado:", estado, "\nTelefone:", telefone, "\nDia:", dia, "\nMês:", mes, "\nAno:", ano);
+
+  res.sendFile(path.join(__dirname, 'views', 'dados.html'));
+});
+
+
+
+const PORT = 8080;
+app.listen(PORT, function () {
+    console.log("app rodando na porta " + PORT);
+})
